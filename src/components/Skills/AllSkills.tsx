b3ef2e5ea36591322ff4e6c098d5c9263d14407d
Pedro Villafuerte <pedro.tvilla@gmail.com>
2025-01-15 18:17:54 -0300

    .

--- src/components/Skills/AllSkills.tsx
+++ src/components/Skills/AllSkills.tsx
@@ -1,12 +1,12 @@
 const allSkills = [
-    {nome:"HTML", logo:"/assets/images/Icons/html.png"},
-    {nome:"CSS", logo:"/assets/images/Icons/css.png"},
-    {nome:"Javascript", logo:"/src/assets/images/Icons/javascript.png"},
-    {nome:"React", logo:"/src/assets/images/Icons/react.png"},
-    {nome:"Tyepscript", logo:"/src/assets/images/Icons/typescript.png"},
-    {nome:"Git", logo:"/src/assets/images/Icons/git.png"},
-    {nome:"Python", logo:"/src/assets/images/Icons/python.png"},
-    {nome:"Selenium", logo:"/src/assets/images/Icons/selenium.png"}
+    {nome:"HTML", logo:"../../../../assets/images/hmtl5Logo"},
+    {nome:"CSS", logo:"src/assets/images/Icons/css.png"},
+    {nome:"Javascript", logo:"src/assets/images/Icons/javascript.png"},
+    {nome:"React", logo:"src/assets/images/Icons/react.png"},
+    {nome:"Tyepscript", logo:"src/assets/images/Icons/typescript.png"},
+    {nome:"Git", logo:"src/assets/images/Icons/git.png"},
+    {nome:"Python", logo:"src/assets/images/Icons/python.png"},
+    {nome:"Selenium", logo:"src/assets/images/Icons/selenium.png"}
 ]
 
 export default allSkills
