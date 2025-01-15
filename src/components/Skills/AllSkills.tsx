f358aa6ab8fcd499e9d8f3960061fcbd25db3eb7
Pedro Villafuerte <pedro.tvilla@gmail.com>
2025-01-15 19:07:02 -0300

    Ajuste imagens skills

--- src/components/Skills/AllSkills.tsx
+++ src/components/Skills/AllSkills.tsx
@@ -1,15 +1,21 @@
 import HTML5Logo from "../../assets/images/Icons/html.png"
 import CssLogo from "../../assets/images/Icons/css.png"
+import JavascriptLogo from "../../assets/images/Icons/javascript.png"
+import ReactLogo from "../../assets/images/Icons/react.png"
+import TypescriptLogo from "../../assets/images/Icons/typescript.png"
+import GitLogo from "../../assets/images/Icons/git.png"
+import PythonLogo from "../../assets/images/Icons/python.png"
+import SeleniumLogo from "../../assets/images/Icons/selenium.png"
 
 const allSkills = [
     {nome:"HTML", logo: HTML5Logo},
     {nome:"CSS", logo: CssLogo},
-    {nome:"Javascript", logo:"src/assets/images/Icons/javascript.png"},
-    {nome:"React", logo:"src/assets/images/Icons/react.png"},
-    {nome:"Tyepscript", logo:"src/assets/images/Icons/typescript.png"},
-    {nome:"Git", logo:"src/assets/images/Icons/git.png"},
-    {nome:"Python", logo:"src/assets/images/Icons/python.png"},
-    {nome:"Selenium", logo:"src/assets/images/Icons/selenium.png"}
+    {nome:"Javascript", logo: JavascriptLogo},
+    {nome:"React", logo: ReactLogo},
+    {nome:"Tyepscript", logo: TypescriptLogo},
+    {nome:"Git", logo: GitLogo},
+    {nome:"Python", logo: PythonLogo},
+    {nome:"Selenium", logo: SeleniumLogo}
 ]
 
 export default allSkills
