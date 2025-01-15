15cfe91c217efab758086f303bbb6c74465fb5fc
Pedro Villafuerte <pedro.tvilla@gmail.com>
2025-01-15 18:27:06 -0300

    .

--- src/components/Skills/AllSkills.tsx
+++ src/components/Skills/AllSkills.tsx
@@ -1,6 +1,6 @@
 const allSkills = [
-    {nome:"HTML", logo:"../../../../assets/images/Icons/html.png"},
-    {nome:"CSS", logo:"src/assets/images/Icons/css.png"},
+    {nome:"HTML", logo:"../src/assets/images/Icons/html.png"},
+    {nome:"CSS", logo:"/src/assets/images/Icons/css.png"},
     {nome:"Javascript", logo:"src/assets/images/Icons/javascript.png"},
     {nome:"React", logo:"src/assets/images/Icons/react.png"},
     {nome:"Tyepscript", logo:"src/assets/images/Icons/typescript.png"},
